a98ff265d492cafcc6c6013b372cb08b5de70519
Carlos Fabian <carlos.fabian@encora.com>
2021-05-21 20:26:28 -0500

    basic CRUD in products

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+
+    @Get()
+    getProducts(){
+        return `These are all the products`
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') id: string){
+        return `This is the product ${id}`
+    }
+
+    @Post()
+    createProduct(@Body() payload : any){
+        return {
+            message: 'Product created',
+            payload
+        }
+    }
+
+    @Put(':id')
+    updateProduct(@Param(':id') id: string, @Body() payload: any){
+        return {
+            id,
+            payload
+        }
+    }
+
+    @Delete(':id')
+    deleteProduct(@Param('id') id: string){
+        return `Product ${id} was deleted`
+    }
+
+}
