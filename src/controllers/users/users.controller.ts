0282c810b7693cf3cc068814ee4ae10aab108510
Carlos Fabian <carlos.fabian@encora.com>
2021-05-21 22:37:27 -0500

    Basic CRUD users

--- src/controllers/users/users.controller.ts
+++ src/controllers/users/users.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()
+    getUsers(){
+        return {
+            data: "These are all the users"
+        }
+    }
+
+    @Get(':id')
+    getUser(@Param('id') id: number){
+        return {
+            data: `This is the product ${id}`
+        }
+    }
+
+    @Post()
+    createUser(@Body() payload: any){
+        return {
+            message: "User was created",
+            payload
+        }
+    }
+
+    @Put(':id')
+    updateUser(@Param('id') id: any, @Body() payload: any ){
+        return {
+            id,
+            payload
+        }
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: string){
+        return {
+            message: `User ${id} was deleted`
+        }
+    }
+}
